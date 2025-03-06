3c8f6050df1b22e9188fa0381875b6ae32f46a3e
Reno-Codes <reno.lulic94@gmail.com>
2025-03-06 02:19:26 +0100

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <HashRouter>
+            <Analytics />
             <App />
         </HashRouter>
     </React.StrictMode>
